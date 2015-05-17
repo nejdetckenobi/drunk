2dc2a7691e5eba513e8f4398527c0fc2564ec194
Nejdet Yucesoy <nejdetyucesoy@gmail.com>
2015-05-17 15:45:22 +0300

    setup.py created. LICENSE file added.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='drunk',
+      version='0.1',
+      description='Weighted Random for Python',
+      author='Nejdet Çağdaş Yücesoy',
+      author_email='nejdetyucesoy@gmail.com',
+      url='',
+      py_modules=['drunk'])
